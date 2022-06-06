fcf1ce3668b8e82ca77af615433ad1828777c19d
Luke Yost <lukexyost@gmail.com>
2022-06-06 11:18:07 -0600

    todos starting to work

--- app/Controllers/TodosController.js
+++ app/Controllers/TodosController.js
@@ -1,10 +1,21 @@
 import { ProxyState } from "../AppState.js";
 import { todosService } from "../Services/TodosService.js";
+import { Todo } from "../Models/Todo.js";
 import { Pop } from "../Utils/Pop.js";
 
+
+function _draw(){
+  let todos = ProxyState.todos
+  let template = ''
+  todos.forEach(t => template += t.Template)
+  document.getElementById('todolist').innerHTML = template
+}
 export class TodosController{
   constructor(){
     console.log('todoin');
+    ProxyState.on('todos', _draw)
+    this.getTodos()
+    // this.makeTodo()
   }
 
   async getTodos(){
@@ -14,6 +25,30 @@ export class TodosController{
     catch(error){
       console.error('to-notin');
       }
+  }
+  async makeTodo(){
+    try{
+      window.event.preventDefault()
+      console.log('making todo');
+      
+      let form = window.event.target
+      let Data = {
+        description: form.name.value
+      }
+      await todosService.makeTodo(Data)
+    }
+    catch(error){
+      console.error(error);
+      }
+  }
+
+  async deleteTodo(id){
+    try{
+      todosService.deleteTodo(id)
+    }
+    catch(error){
+      console.error(error);
+    }
   }
 
 }
--- app/Services/TodosService.js
+++ app/Services/TodosService.js
@@ -1,14 +1,27 @@
 import { ProxyState } from "../AppState.js";
-// import { Todo } from "../Models/Todo"
+import { Todo } from "../Models/Todo.js";
+
 
 let todoAPI = axios.create({
-  baseURL: 'https://bcw-sandbox.herokuapp.com/api/LukeYost/todos',
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/LukeYost/todos/',
   timeout: 10000
 })
 
 class TodosService{
+  async getTodos(){
+    const res = await todoAPI.get()
+    console.log('get todo', res.data);
+    ProxyState.todos = res.data.map(t => new Todo(t))
+    console.log(ProxyState.todos, res.data);
+  }
 
+
+  async makeTodo(data){
+    const res = await todoAPI.post('',data)
+    console.log('making todo', res.data);
+    ProxyState.todos = [...ProxyState.todos, new Todo(res.data)]
+  }
+
+    
 }
-
-
 export const todosService = new TodosService()
